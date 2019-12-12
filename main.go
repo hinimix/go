b8e8cf9cc7aecd698adc3cf0f7428ca4e1a3a892
guanhuai <guanhuai@trgroup.cn>
2019-12-12 16:44:16 +0800

    import from another file

--- main.go
+++ main.go
@@ -1,18 +1,19 @@
 package main
-// usage: go run src/test.go -name="dsb"
+
+// usage: go run test.go -name="dsb"
 import (
 	//参数化引用包
 	"flag"
-	"fmt"
 )
 
 var name string
 
-func init () {
+func init() {
 	flag.StringVar(&name, "name", "everyone", "the greeting object")
 }
 
 func main() {
 	flag.Parse()
-	fmt.Printf("hello %s\n", name)
+	//fmt.Printf("hello %s\n", name)
+	hello(name)
 }
