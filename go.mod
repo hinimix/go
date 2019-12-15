3d0b2165e0230cb518e3485b2f0f1c319e467c4d
guanhuai <guanhuai@trgroup.cn>
2019-12-15 17:50:21 +0800

    type change
    Signed-off-by: guanhuai <guanhuai@trgroup.cn>

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module test_go
 
 go 1.13
+
+require github.com/hyper0x/Golang_Puzzlers v0.0.0-20190727034740-0dc31f97cb7f // indirect
--- main.go
+++ main.go
@@ -1,22 +1,26 @@
+
 package main
 
-// usage: go run test.go -name="dsb"
 import (
-	//参数化引用包
-	"flag"
-	//lib567 is alias
-	lib567 "test_go/lib"
+  "flag"
+  "fmt"
 )
 
-var name string
-
-func init() {
-	flag.StringVar(&name, "name", "everyone", "the greeting object")
+func getTheFlag() *string {
+  return flag.String("name", "everyone","greeting object")
 }
 
 func main() {
-	flag.Parse()
-	//fmt.Printf("hello %s\n", name)
-	lib567.Hello(name)
-	lib567.Hello("dad")
+
+  var i16 int16
+  i16 = -255
+  var i8 int8
+  i8 = int8(i16)
+  fmt.Printf("i8 value:  %v!\n", i8)
+  fmt.Printf("i16 value:  %v!\n", i16)
+  fmt.Printf("error: %v!\n", string(-221))
+  var aaa rune
+  aaa = 23321
+  fmt.Println("aaa value", aaa)
+
 }
